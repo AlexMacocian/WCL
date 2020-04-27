78e78e8241746c47f9aad3a3cd70f9b2ddf4ba6d
Alexandru Macocian <almacoci@microsoft.com>
2020-04-27 21:35:16 +0200

    Add IsExpanded Property to ExpandingPanel

--- WCL/ExpandingPanel.cs
+++ WCL/ExpandingPanel.cs
@@ -7,11 +7,20 @@ namespace WCL
     {
         public static readonly DependencyProperty TextProperty =
         DependencyProperty.Register("Text", typeof(string), typeof(ExpandingPanel), new UIPropertyMetadata(null));
+        
+        public static readonly DependencyProperty IsExpandedProperty =
+        DependencyProperty.Register("IsExpanded", typeof(bool), typeof(ExpandingPanel), new UIPropertyMetadata(null));
 
         public string Text
         {
-            get { return (string)GetValue(TextProperty); }
-            set { SetValue(TextProperty, value); }
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public bool IsExpanded
+        {
+            get => (bool)GetValue(IsExpandedProperty);
+            set => SetValue(IsExpandedProperty, value);
         }
 
         static ExpandingPanel()
