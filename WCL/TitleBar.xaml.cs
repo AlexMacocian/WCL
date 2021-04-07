d887124595bc01190ebe31db385e6a9d1d378299
Alexandru Macocian <almacoci@microsoft.com>
2021-04-07 11:36:15 +0200

    Fix titlebar bindings. Properties to hide titlebar buttons.

--- WCL/TitleBar.xaml.cs
+++ WCL/TitleBar.xaml.cs
@@ -21,6 +21,11 @@ namespace WCL
     /// </summary>
     public partial class TitleBar : UserControl
     {
+        public static DependencyProperty CloseButtonVisibleProperty = DependencyProperty.Register(nameof(CloseButtonVisible), typeof(bool), typeof(TitleBar), new PropertyMetadata(true));
+        public static DependencyProperty ResizeButtonVisibleProperty = DependencyProperty.Register(nameof(ResizeButtonVisible), typeof(bool), typeof(TitleBar), new PropertyMetadata(true));
+        public static DependencyProperty MinimizeButtonVisibleProperty = DependencyProperty.Register(nameof(MinimizeButtonVisible), typeof(bool), typeof(TitleBar), new PropertyMetadata(true));
+        public static DependencyProperty TitleProperty = DependencyProperty.Register(nameof(Title), typeof(string), typeof(TitleBar));
+
         private WindowState windowState = WindowState.Normal;
 
         public event EventHandler Clicked, CloseButtonClicked, RestoreButtonClicked, MaximizeButtonClicked, MinimizeButtonClicked;
@@ -48,10 +53,10 @@ namespace WCL
         /// <summary>
         /// String that is displayed on the left side of the titlebar.
         /// </summary>
-        public string Text
+        public string Title
         {
-            get => TitleBlock.Text;
-            set => TitleBlock.Text = value;
+            get => (string)this.GetValue(TitleProperty);
+            set => this.SetValue(TitleProperty, value);
         }
         public UIElement Icon
         {
@@ -62,10 +67,34 @@ namespace WCL
                 IconContainer.Children.Add(value);
             }
         }
+        public bool CloseButtonVisible
+        {
+            get => (bool)this.GetValue(CloseButtonVisibleProperty);
+            set => this.SetValue(CloseButtonVisibleProperty, value);
+        }
+        public bool ResizeButtonVisible
+        {
+            get => (bool)this.GetValue(ResizeButtonVisibleProperty);
+            set => this.SetValue(ResizeButtonVisibleProperty, value);
+        }
+        public bool MinimizeButtonVisible
+        {
+            get => (bool)this.GetValue(MinimizeButtonVisibleProperty);
+            set => this.SetValue(MinimizeButtonVisibleProperty, value);
+        }
 
         public TitleBar()
         {
             InitializeComponent();
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+            if (e.Property == ResizeButtonVisibleProperty)
+            {
+                this.ManageResizeButtonsVisibility();
+            }
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
@@ -93,6 +122,28 @@ namespace WCL
             if(e.ChangedButton == MouseButton.Left)
             {
                 Clicked?.Invoke(this, e);
+            }
+        }
+
+        private void ManageResizeButtonsVisibility()
+        {
+            if (this.ResizeButtonVisible is false)
+            {
+                this.RestoreButton.Visibility = Visibility.Collapsed;
+                this.MaximizeButton.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                if (this.WindowState == WindowState.Normal)
+                {
+                    this.RestoreButton.Visibility = Visibility.Collapsed;
+                    this.MaximizeButton.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    this.RestoreButton.Visibility = Visibility.Visible;
+                    this.MaximizeButton.Visibility = Visibility.Collapsed;
+                }
             }
         }
     }
